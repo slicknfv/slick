46176d7c17c2df47313191302b2a094f9dac7fad
Bilal Anwer <bilal@cc.gatech.edu>
2013-07-13 19:53:52 +0000

    Checking in scp.py module; using code from here: https://github.com/jbardin/scp.py

---
+++ archive/scp.py/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+setup(
+        name = 'scp',
+        version = '0.5.1',
+        author = 'James Bardin',
+        author_email = 'j.bardin@gmail.com',
+        license = 'LGPL',
+        url = 'https://github.com/jbardin/scp.py.git',
+        description='scp module for paramiko',
+        py_modules = ['scp'],
+        install_requires = ['paramiko'],
+)
