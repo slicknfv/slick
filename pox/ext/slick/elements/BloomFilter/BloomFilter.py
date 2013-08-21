a404ea59070976440d221d8f6457783500453095
Bilal Anwer <bilal@cc.gatech.edu>
2013-08-21 03:38:58 +0000

    BloomFilter example code

--- pox/ext/slick/elements/BloomFilter/BloomFilter.py
+++ pox/ext/slick/elements/BloomFilter/BloomFilter.py
@@ -1,87 +1,98 @@
 import os.path
-import dpkt
+import logging
 import socket
+
+import dpkt
 import pybloomfilter
+
 from slick.Element import Element
 
 class BloomFilter(Element):
-    def __init__(self, shim, ed):
-        Element.__init__(self, shim, ed )
-	# Name of the file where sentinels are sored.
-        self.filename = None
-	# bloomfilder size.
-        self.bf_size = None
-        self.error_rate = None
-	# Handle for the pybloomfilter.BloomFilter class
-        self.lib_object = None
+  """Example Element that can be used to match on TCP port field."""
 
-    def init(self, ed, params):
-	"""Sartup the element.
-	
-	Args:
-	  ed: Name of the element descriptor.
-	  params: Its a <key,value> dictionary that has params
-		required by element to startup.
-	Returns:
-	   None
-	"""
-        if(params.has_key("bf_size")):
-            self.bf_size = params["bf_size"]
-        if(params.has_key("error_rate")):
-            self.error_rate = params["error_rate"]
-        self._bf_init(self.bf_size,self.error_rate,self.filename)
+  def __init__(self, shim, ed):
+    Element.__init__(self, shim, ed )
+    # Name of the file where sentinels are stored.
+    self.filename = None
+    # bloomfilder size and error rate
+    self.bf_size = None
+    self.error_rate = None
+    # Handle for the pybloomfilter.BloomFilter class
+    self.lib_object = None
 
-    def configure(self, params):
-	"""Update element state accoeding to user params.
+  def init(self, params):
+    """Sartup the element.
 
-	Args:
-	  params: Its a map of key and values.	
-	"""
-        if(params.has_key("sentinelfile")):
-            self.filename = params["sentinelfile"]
-        if(params.has_key("addsentinel")):
-            sentinel = params["addsentinel"]
-            if(self.lib_object):
-                self.lib_object.add(sentinel) # Add the new value to the sentinel
+    Args:
+      ed: Name of the element descriptor.
+      params: Its a <key,value> dictionary that has params
+      required by element to startup.
+    Returns:
+       None
+    """
+    if params.has_key("bf_size"):
+      self.bf_size = params["bf_size"]
+    if params.has_key("error_rate"):
+      self.error_rate = params["error_rate"]
+    self._bf_init(self.bf_size, self.error_rate, self.filename)
 
-    def _bf_init(self, bf_size, error_rate, filename):
-	"""Here we create the bloomfilter object."""
-        self.lib_object = pybloomfilter.BloomFilter(10000, 0.001, 'filter.bloom')
-        f = open("../bloomfilter/fieldvals") #One value per line.
-        for val in f:
-            logging.info("Inserting value into the bloom fileter: %s",val)
-            self.lib_object.add(val.rstrip())
+  def configure(self, params):
+    """Update element state accoeding to user params.
 
-    def process_pkt(self, buf):
-	"""Do packet processing."""
-        trigger = {"ed":self.ed}
-        eth = dpkt.ethernet.Ethernet(buf)
-        flow = self.extract_flow(eth)
-        pkt_len = len(buf)
-        if(eth.type == dpkt.ethernet.ETH_TYPE_IP):
-            ip = eth.data
-            dst_ip = socket.inet_ntoa(ip.dst)
-            src_ip = socket.inet_ntoa(ip.src)
-            if(ip.p == dpkt.ip.IP_PROTO_TCP):
-                tcp =ip.data
-                #bloom filters value to match
-                val = str(tcp.dport)
-                #Check if value is present in the filter
-                val_present = val in self.lib_object
-		logging.info('Matched the port number: %s', val)
-                if(val_present):
-                    trigger["type"] = "trigger"
-                    trigger["BF_trigger_type"] = "VAL_DETECTED"
-		    # Call base class raise trigger.
-                    self.raise_trigger(trigger)
-        # This must be called.
-        self.fwd_pkt(buf)
-                        
-#Testing
-def main():
-    bf = BloomFilter(None)
-    bf.init(100,{})
-    print bf.ed
+    Args:
+      params: Its a map of key and values.	
+    """
+    if params.has_key("sentinelfile"):
+      self.filename = params["sentinelfile"]
+    if params.has_key("addsentinel"):
+      sentinel = params["addsentinel"]
+      if self.lib_object:
+        self.lib_object.add(sentinel) # Add the new value to the sentinel
 
-if __name__ == "__main__":
-    main()
+  def _bf_init(self, bf_size, error_rate, filename='filter.bloom'):
+    """Here we create the bloomfilter object.
+
+    Args:
+        bf_size: Bloom Filter size.
+        error_rate: Error Rate allowed.
+        filename: Name of the file.
+    Returns:
+        None
+    """
+    self.lib_object = pybloomfilter.BloomFilter(10000, 0.001, filename)
+    # TODO:Clean up paths.
+    with open("../pox/ext/slick/elements/BloomFilter/fieldvals",'r') as f: #One value per line.
+      for val in f:
+        logging.info("Inserting value into the bloom filter: %s",val)
+        self.lib_object.add(val.rstrip())
+
+  def process_pkt(self, buf):
+    """Match the TCP port using bloom filters.
+
+    Args:
+        buf: Packet buffer.
+    Returns:
+        None
+    """
+    trigger = {"ed": self.ed}
+    flow = self.extract_flow(buf)
+    eth = dpkt.ethernet.Ethernet(buf)
+    pkt_len = len(buf)
+    if(eth.type == dpkt.ethernet.ETH_TYPE_IP):
+      ip = eth.data
+      dst_ip = socket.inet_ntoa(ip.dst)
+      src_ip = socket.inet_ntoa(ip.src)
+      if(ip.p == dpkt.ip.IP_PROTO_TCP):
+        tcp =ip.data
+        #bloom filters value to match
+        val = str(tcp.dport)
+        #Check if value is present in the filter
+        val_present = val in self.lib_object
+        logging.info('Matched the port number: %s', val)
+        if(val_present):
+          trigger["type"] = "trigger"
+          trigger["BF_trigger_type"] = "VAL_DETECTED"
+          # Call base class raise trigger.
+          self.raise_trigger(trigger)
+    # This must be called to forward the packet.
+    self.fwd_pkt(buf)
