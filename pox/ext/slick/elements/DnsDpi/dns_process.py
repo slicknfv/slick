dae2fee857b4e68e6c161147d98a4f2b12ed75f3
Bilal Anwer <bilal@cc.gatech.edu>
2013-08-26 09:36:35 +0000

    Cleaning up

--- pox/ext/slick/elements/DnsDpi/dns_process.py
+++ pox/ext/slick/elements/DnsDpi/dns_process.py
@@ -24,59 +24,16 @@ from loadcache import LoadCache
 
 from handledns import HandleDNS
 
-DEBUG_COLLECTION = False
 class DNSProcess:
     def __init__(self):
         self.load_cache = LoadCache()
         self.load_cache.load_files() # need to do this during init as its part of init.
         self.dns_handler = HandleDNS(self.load_cache)
 
-#    # --
-#    # Opens and gives a handle of pcap file.
-#    # --
-#    def loadpcap(self):
-#        print self.filename
-#        f = open(self.filename)
-#        self.pcap_file = dpkt.pcap.Reader(f)
-#        self.decode(self.pcap_file)
-    
-
-    # --
-    # This function is used to sniff from wire
-    # --
-    #def sniff(self):
-    #    # For now sniffing on the Ethernet interface.
-    #    # sniffing on "any" causes the packets to be received in cooked form
-    #    # which looses the Ethernet frame information but gives rest of the information
-    #    # For further details: http://wiki.wireshark.org/SLL or man page of packet 7
-    #    #pc = pcap.pcap(self.iface)
-    #    pc = pcap.pcap("eth0")
-    #    print 'Listening on %s: With filter %s' % (pc.name, pc.filter)
-    #    try:
-    #        decode = {  pcap.DLT_LOOP:dpkt.loopback.Loopback,
-    #                    pcap.DLT_NULL:dpkt.loopback.Loopback,
-    #                    pcap.DLT_IEEE802:dpkt.ethernet.Ethernet,
-    #                    pcap.DLT_EN10MB:dpkt.ethernet.Ethernet,
-    #                    pcap.DLT_LINUX_SLL:dpkt.sll.SLL}[pc.datalink()]
-    #    except KeyError:
-    #        print pc.datalink()
-    #        print "Please check if you are handling proper packet type"
-    #    pass
-    #    try:
-    #        self.decode(pc)
-    #    except KeyboardInterrupt:
-    #        nrecv, ndrop, nifdrop = pc.stats()
-    #        print '\n%d packets received by filter' % nrecv
-    #        print '%d packets dropped by kernel' % ndrop
-            
-    
-
-
-    # --
-    # This function is used to decode the packets received from wire
-    # This should be called by process_pkt
-    # --
     def decode(self,buf):
+        """ This function is used to decode the packets received from wire.
+        It should be called by process_pkt
+        """
         eth = dpkt.ethernet.Ethernet(buf)
         pkt_len = len(buf)
         if(eth.type== dpkt.ethernet.ETH_TYPE_IP):
@@ -91,21 +48,17 @@ class DNSProcess:
                     break
             if((octet_list[0] == "224") or (octet_list[0] == "239")):
                 broadcast = True #Its multicast actually.
-            
             if not broadcast:
                 if(ip.p == dpkt.ip.IP_PROTO_TCP):
                     pass
                 elif(ip.p == dpkt.ip.IP_PROTO_UDP):
                     udp =ip.data
-                    #print "dst port:", udp.dport
                     if((udp.dport == 53) or (udp.sport == 53)): # A request. 
                         if(udp.dport == 53): # A request. 
-                            print "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX BILALLLLLLLLLLLLL"
                             return self.dns_handler.handle_dns_request(ip.src,ip.dst,ip.p,udp.sport,udp.dport,udp.data)
                         if(udp.sport == 53): # A DNS response
                             self.dns_handler.handle_dns_response(ip.src,ip.dst,ip.p,udp.sport,udp.dport,udp.data)
                     else:
-                        #ip_list = self.dns_data.get_connected_ip(src_mac)#Return the list of ip addresses returned tou
                         pass
 
 
