9cc272a5ea71fc984d633ae14a9f6fc5856b96f5
Bilal Anwer <bilal@cc.gatech.edu>
2013-11-06 15:45:11 +0000

    Adding skeleton files for topology creation and format of the data.

---
+++ mininet_scripts/build_topology.py
@@ -0,0 +1,13 @@
+###########################################################
+# Given a topology file with certain format
+# build a network topology from it that can be used
+# by mininet.
+###########################################################
+
+def read_topo(topo_file):
+    pass
+
+
+def build_topo():
+    """Reads the topology in csv and returns a mininet Topo object."""
+    pass
