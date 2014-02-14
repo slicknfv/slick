db9a559e30d88e1cde8796036a8df7c05361da1f
Bilal Anwer <bilal@cc.gatech.edu>
2014-02-14 17:33:07 +0000

    Adding affinity option in for testing. And adding proto option to avoid the WARNINGS from POX controller.

--- pox/ext/slick/apps/DirectionLogger.py
+++ pox/ext/slick/apps/DirectionLogger.py
@@ -2,6 +2,7 @@
     DirectionLogger: One Application, Two Element Instance.
     For first element instance we tell controller to only use one direction while traversing the middlebox1.
     For second element instance we tell controller to use both directions traversing the middlebox2.
+    This tests unidirection/bidirection and path affinity
 """
 from slick.Application import Application
 
@@ -12,15 +13,17 @@ class DirectionLogger(Application):
     def init(self):
         # Start the Logger
         parameters = [{"file_name":"/tmp/unidirection_log"}]
-        admin_params = [{"bidirection":False}]
+        admin_params = [{"bidirection":False,"affinity":True}]
         flow = self.make_wildcard_flow()
         flow['tp_dst'] = 53
+        flow['nw_proto'] = 17
         ed1 = self.apply_elem( flow, ["Logger"], parameters, admin_params) 
 
         parameters = [{"file_name":"/tmp/bidirection_log"}]
-        admin_params = [{"bidirection":True}]
+        admin_params = [{"bidirection":True, "affinity":True}]
         flow = self.make_wildcard_flow()
         flow['tp_dst'] = 53
+        flow['nw_proto'] = 17
         ed2 = self.apply_elem( flow, ["Logger1"], parameters, admin_params) 
 
         if(self.check_elems_installed(ed1) and self.check_elems_installed(ed2)):
